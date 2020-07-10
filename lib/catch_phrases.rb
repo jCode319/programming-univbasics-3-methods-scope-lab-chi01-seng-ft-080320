6674ba4e5f3e6c4ff6f9e9f97b80f843394e977d
Jarrell Jackson <jdjack3@gmail.com>
2020-07-10 18:12:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -15,6 +15,7 @@ def link
 end
 
 def all_phrases
-  all_phrases = 'mario'
-  puts all_phrases
+  puts all_phrases = "It's-a me, Mario!"
+  puts all_phrases = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts all_phrases = "It's Dangerous To Go Alone! Take This."
 end
